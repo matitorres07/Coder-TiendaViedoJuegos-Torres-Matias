28a208c7320f015e235131eaa10d66d116fdf4de
matitorres07 <matitorres07@gmail.com>
2022-04-15 14:39:05 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
-    <h1>Primera app con react!</h1>
+      <NavBar/>
+    <h1 className="text-3xl font-bold underline">Primera app con react!</h1>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const NavBar = () => {
+  const estilos ={
+    border:'solid red 1px',
+    color: 'blue'
+  }
+  return (
+    <div className="navbar bg-base-300">
+  <div className="flex-1">
+  <img src="https://cdn-icons-png.flaticon.com/512/3408/3408506.png" alt="" className="object-scale-down h-30 w-11 ..."/>
+  <div className="flex-row">
+    <ul className="menu menu-horizontal p-0">
+      <li><a>Tienda</a></li>
+      <li tabindex="0">
+        <a>
+          Consolas
+          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+        </a>
+        <ul className="p-2 bg-base-100">
+          <li><a>PS4</a></li>
+          <li><a>XBOX</a></li>
+          <li><a>SWITCH</a></li>
+        </ul>
+      </li>
+      <li><a>Nosotros</a></li>
+    </ul>
+  </div>
+  </div>
+  
+  <div className="flex-none">
+    <div className="dropdown dropdown-end">
+      <label tabindex="0" className="btn btn-ghost btn-circle">
+        <div className="indicator">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+          <span className="badge badge-sm indicator-item">8</span>
+        </div>
+      </label>
+      <div tabindex="0" className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+        <div className="card-body">
+          <span className="font-bold text-lg">Cantidad!</span>
+          <span className="text-info">Subtotal: $$$$</span>
+          <div className="card-actions">
+            <button className="btn btn-primary btn-block">Ir al carrito!</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="dropdown dropdown-end">
+      <label tabindex="0" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img src="https://cdn-icons-png.flaticon.com/512/727/727368.png" />
+        </div>
+      </label>
+      <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default NavBar
